a6a043dc5f5a2f9e13ab738c55f3966c96e8ad0a
Lydia Joynes <lydiajoynesgmail.com>
2019-12-30 17:25:26 +0000

    image update to png

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,4 @@
 import React from 'react';
-import vegan from './images/vegan.jpeg';
-import hala from './images/hala.jpeg';
-import glutenfree from './images/glutenfree.png';
-import vegetarian from './images/vegetarian.jpeg';
-import pesca from './images/pesca.png';
 import './styles/Header.css';
 
 
@@ -11,63 +6,11 @@ class Header extends React.Component {
   render() {
     return (
       <div>
-        <h1 id="mainHeading">FOODERS</h1>
-        <p>We help you navigate the ever changing food scene. Your best friend now joined the V-gang? No problem! </p>
-        <p>Your cousin has recently been diagnosed with coeliac? You only eat fish?</p>
-        <p>FOODERS are especially for you</p>
-        <br />
-        <br />
-        <div class="container">
-          <div class="row">
-            <div class="col-sm">
-              <img src={hala} alt="halal" class="rounded-circle" />
-              <br />
-              Halal
-      <div class="input">
-                <input type="checkbox" />
-              </div>
-            </div>
-            <div class="col-sm">
-              <img src={pesca} alt="Pescatarian" class="rounded-circle" />
-              <br />
-              Pescatarian
-      <div class="input">
-                <input type="checkbox" />
-              </div>
-            </div>
-            <div class="col-sm">
-              <img src={glutenfree} alt="Gluten Free" class="rounded-circle"></img>
-              <br />
-              Gluten Free
-      <div class="input">
-                <input type="checkbox" />
-              </div>
-            </div>
-            <br />
-            <br />
-            <div class="row">
-              <div class="col">
-                <img src={vegan} alt="vegan" class="rounded-circle" />
-                <br />
-                Vegan
-        <div class="input">
-                  <input type="checkbox" />
-                </div>
-              </div>
-              <div class="col">
-                <img src={vegetarian} alt="vegaetarian" class="rounded-circle" />
-                <br />
-                vegetarian
-        <div class="input">
-                  <input type="checkbox" />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <br />
-        <br />
-        <button type="button" class="btn btn-primary btn-lg">Restaurant Listing</button>
+        <h1 id="mainHeading">FOODER</h1>
+        <p>Fooder is here to help you with yur dining needs!
+          Does your friendship group have several dietary needs, but you still wish to dine together?
+          Use our simple app below to get recommendations on where to eat.
+        </p>
       </div>
     )
   };
