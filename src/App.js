0272f325f082c499305eea853fa508d4f3d6bfa3
Lydia Joynes <lydiajoynesgmail.com>
2020-01-01 16:23:59 +0000

    implemented install of axios and dietary options GET request

--- src/App.js
+++ src/App.js
@@ -2,12 +2,26 @@ import React from 'react';
 import Booking from './Booking';
 import Header from './Header';
 import Restaurants from './Restaurants';
+import axios from "axios";
 import Dietary from './Dietary';
 import Restfilter from './Restfilter';
 import "./styles/App.css";
-
-
 class App extends React.Component {
+  state = {
+    dietaryOptions: []
+  };
+  componentDidMount() {
+    axios.get("https://41cpd3sfbg.execute-api.eu-west-2.amazonaws.com/dev/dietaryOptions")
+      .then((response) => {
+        const dietaryOptions = response.data;
+        this.setState({
+          dietaryOptions: dietaryOptions
+        });
+      })
+      .catch((err) => {
+        console.log("Error getting task data", err);
+      });
+  }
   render() {
     return (
       <div>
