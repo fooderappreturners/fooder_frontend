71adf6d0a9270f3d0eeabd8bc5a04babcf618622
Lydia Joynes <lydiajoynesgmail.com>
2020-01-01 21:18:02 +0000

    implemented API frameworks to frontend and installed moment

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import axios from "axios";
+import moment from "moment";
 import Booking from './Booking';
 import Header from './Header';
 import Restaurants from './Restaurants';
-import axios from "axios";
 import Dietary from './Dietary';
 import Restfilter from './Restfilter';
 import "./styles/App.css";
+
+const API_URL = "https://41cpd3sfbg.execute-api.eu-west-2.amazonaws.com/dev/";
+
 class App extends React.Component {
   state = {
     dietaryOptions: []
   };
   componentDidMount() {
-    axios.get("https://41cpd3sfbg.execute-api.eu-west-2.amazonaws.com/dev/dietaryOptions")
+    axios.get(`${API_URL}dietaryOptions`)
       .then((response) => {
         const dietaryOptions = response.data;
         this.setState({
@@ -21,6 +25,60 @@ class App extends React.Component {
       .catch((err) => {
         console.log("Error getting task data", err);
       });
+  }
+
+  filterRestaurants =
+    handleClick = (event) => {
+      event.preventDefault();
+      axios.get(`${API_URL}restaurants` + id)
+    }
+  addNewBooking = () => {
+    const bookingsCopy = this.state.bookings.slice();
+    const newBooking = {
+      name: "",
+      date: '',
+      number: ''
+    };
+    axios.post(`${API_URL}addBooking`, newBooking)
+      .then((response) => {
+        const bookingFromDB = response.data;
+        bookingsCopy.push(bookingFromDB)
+        console.log(response)
+        this.setState({
+          bookings: bookingsCopy
+        })
+      })
+      .catch((err) => {
+        console.log("Error creating task", err);
+      })
+  };
+
+  bookings = () => {
+    axios.get(`${API_URL}bookings`)
+      .then((response) => {
+        const bookings = response.data;
+        this.setState({
+          bookings: bookings
+        });
+      })
+      .catch((err) => {
+        console.log("Error getting task data", err);
+      });
+  }
+
+  deleteTask = id => {
+    axios.delete(`${API_URL}deleteBooking` + id)
+      .then((response) => {
+        const bookingConfirmations = this.state.bookings.filter(task => {
+          return task.taskId !== id
+        });
+        this.setState({
+          bookings: bookingConfirmations
+        });
+      })
+      .catch((err) => {
+        console.log("Error deleting task", err);
+      })
   }
   render() {
     return (
--- src/Restfilter.js
+++ src/Restfilter.js
@@ -2,6 +2,24 @@ import React from 'react';
 import './styles/Restfilter.css';
 
 class Button extends React.Component {
+    state = {
+        dietarySelection: "",
+    };
+
+    restaurantDietSelection = (event) => {
+        console.log(event.target.value)
+        this.setState({
+            dietarySelection: event.target.value
+        });
+    }
+
+    handleClick = (event) => {
+        event.preventDefault();
+        this.props.dietarySelection(this.state.dietarySelection);
+        this.setState({
+            dietarySelection: ""
+        });
+    }
     render() {
         return (
             <div className="container">
