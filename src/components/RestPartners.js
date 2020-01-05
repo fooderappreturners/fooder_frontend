19e514278b3c6f90cf296af629893fbed28632a3
Lydia Joynes <lydiajoynesgmail.com>
2020-01-05 20:14:59 +0000

    added date selector, party size and button

--- src/components/RestPartners.js
+++ src/components/RestPartners.js
@@ -11,13 +11,30 @@ export default (props) => {
                     <img className="restPic" src={img} alt={name} />
                 </div>
             </div>
-            <div className="col-8">
+            <div className="col-6">
                 <h1>{name}</h1>
                 <p className="desc">{description}</p>
                 <p className="address">Address: {location}</p>
                 <a className="number" href={`tel:${phoneNumber}`} title={`Call us on: ${phoneNumber}`}>Tel: {phoneNumber}</a>
             </div>
-
+            <div className="row datebook">
+                <div className="col">
+                    <div className="form-group mx-sm-3 mb-2">
+                        <input type='date' />
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="exampleFormControlSelect1">Party Size</label>
+                    <select class="form-control" id="exampleFormControlSelect1">
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5+</option>
+                    </select>
+                </div>
+                <button type="button" class="btn btn-primary">Book</button>
+            </div>
         </div>
     )
 } 
